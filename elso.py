c7e17f353df4a286885fd6a0fadc8fd3cc77d9fe
pinczesimark <pinmark06@gmail.com>
2022-09-19 10:11:38 +0200

    harmaddig kommit

--- elso.py
+++ elso.py
@@ -1,10 +1,21 @@
 from os import system
-from selectors import EpollSelector
+from os import system
 
 system('cls')
 
 elsovaltozo = 12
+ 
+ #string konkatenáció
+kiir_1 = "A változó értéke: " + str(elsovaltozo)
+print(kiir_1)
 
-kiir = "A változó értéke: " + str(elsovaltozo)
+#fomattált sztring 1.verzió
+kiir_1 = "A változó értéke: {}"  .format(elsovaltozo)
+print(kiir_1)
 
-print(kiir)
+#formattált sztring 2.verzió
+kiir_1 = f"A változó értéke: " (elsovaltozo)
+print(kiir_1)
+
+kiir_1 ="A változás értéke {0}" .format(elsovaltozo)
+print(kiir_1)
